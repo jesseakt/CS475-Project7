360972827ef0f71091aa54453cfe5616d33f949d
jesseakt <thorenje@oregonstate.edu>
2017-06-11 14:08:51 -0700

    Finished openCL

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # CS475-Project7
+
+A collection of files to examine autocorrelation.
+Includes an implementation in openMP, simd, and openCL
--- p7openCL/testScript.py
+++ p7openCL/testScript.py
@@ -1,6 +1,6 @@
 #Testing Script for CS475 Project 7b
 #Jesse Thoren
-#Description: Compiles and runs project7.cpp
+#Description: Compiles and runs project7.cpp (RABBIT ONLY)
 
 import os
 cmd = "g++ -o project7 project7.cpp ./libOpenCL.so -lm -fopenmp"
